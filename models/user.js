7226d246b0d64f9bb65a1ec2d5f0809c0e2c96f9
matoskni <matoskni@student.jyu.fi>
2021-10-16 17:50:30 +0300

    added password hashing

--- models/user.js
+++ models/user.js
@@ -5,3 +5,28 @@ const UserSchema = new mongoose.Schema({
     username: String,
     password: String
 })
+
+UserSchema.pre("save", function (next) {
+    const user = this
+  
+    if (this.isModified("password") || this.isNew) {
+      bcrypt.genSalt(10, function (saltError, salt) {
+        if (saltError) {
+          return next(saltError)
+        } else {
+          bcrypt.hash(user.password, salt, function(hashError, hash) {
+            if (hashError) {
+              return next(hashError)
+            }
+  
+            user.password = hash
+            next()
+          })
+        }
+      })
+    } else {
+      return next()
+    }
+  })
+
+module.exports = mongoose.model('User', UserSchema)
